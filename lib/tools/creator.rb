b49e21608ec894fcd71c430a6d110a5fd1898da9
Andrew Page <andrew@andrewpage.me>
2015-12-23 19:01:39 -0800

    Add documentation for Creator

--- lib/tools/creator.rb
+++ lib/tools/creator.rb
@@ -4,6 +4,14 @@ module Tools
   class Creator
     attr_reader :files, :announce, :source, :output, :piece_size, :privacy
 
+    # Tools::Creator creates .torrent files for all files found in its manifest.
+    #
+    # @param manifest Path to JSON configuration file that lists all files to be proceesed.
+    # @param announce Announce URL of the primary tracker for this torrent.
+    # @param source Path to the directory where source files will be located.
+    # @param output Path to the directory where completed .torrent files will be saved.
+    # @param piece_size BitTorrent piece size, as a power of 2. e.g. 18 = piece size of 256kb because 2**18 is 256kb
+    # @param privacy Should this torrent be private?
     def initialize(manifest:, announce:, source:, output:, piece_size:, privacy:)
       @announce = announce
       @source = source
@@ -31,6 +39,7 @@ module Tools
     private
 
     # Extract list of files to process from manifest JSON
+    # @param manifest Path to JSON configuration file that lists all files to be proceesed.
     def extract_files(manifest)
       contents = File.read(manifest)
       json = JSON.parse(contents)
@@ -38,12 +47,15 @@ module Tools
       json['files']
     end
 
+    # Creates .torrent output directory if it does not yet exist.
     def create_output_directory
-      Dir.mkdir(output) unless File.exists?(output)
+      Dir.mkdir_p(output) unless File.exists?(output)
     end
 
     # Create the .torrent file
-    def mktorrent(name, path)
+    # @param name Name of the completed .torrent file.
+    # @param source Path to the source directory for this torrent.
+    def mktorrent(name, source)
       outfile = File.join(output, name)
 
       cmd = Array.new
@@ -51,7 +63,7 @@ module Tools
       cmd << %(-l #{piece_size})
       cmd << %(-p) if privacy
       cmd << %(-a #{announce})
-      cmd << %("#{path}")
+      cmd << %("#{source}")
       cmd << %(-o "#{outfile}")
       cmd = cmd.join(' ')
 
