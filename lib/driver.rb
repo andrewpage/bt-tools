16230be06376d6cd211ff9fb92294d74f8432923
Andrew Page <andrew@andrewpage.me>
2015-12-23 18:22:02 -0800

    Add basic command validation

--- lib/driver.rb
+++ lib/driver.rb
@@ -3,11 +3,15 @@ require 'optionparser'
 
 class ToolsDriver
   DEFAULT_CONFIGURATION_PATH = File.join('config', 'configuration.json')
+  AVAILABLE_COMMANDS = %i(create transcode)
 
   attr_reader :options
 
+  # @param args: ARGV
   def initialize(args)
     @args = args
+
+    # Set options defaults
     @options = OpenStruct.new(
       configuration_path: DEFAULT_CONFIGURATION_PATH
     )
@@ -15,26 +19,41 @@ class ToolsDriver
     parse
   end
 
+  # Perform actions based on command & options
   def execute
     puts options.command
   end
 
   private
 
+  # Extract command + arguments from ARGV
   def parse
+    # Parse all arguments first
     parser.parse!(@args)
 
     # Get the first argument, this is our command
-    command = @args.pop
-    raise OptionParser::MissingArgument, 'command' unless command
+    cmd = @args.pop
+    raise OptionParser::MissingArgument, 'command' unless cmd
 
     # Set the command if it's present
-    options.command = command.to_sym
+    options.command = cmd.to_sym
+
+    raise "Invalid command. Available commands: #{AVAILABLE_COMMANDS.join(', ')}." unless valid_command?
   end
 
+  def command
+    options.command
+  end
+
+  # Is this command supported?
+  def valid_command?
+    AVAILABLE_COMMANDS.include?(command)
+  end
+
+  # CLI option parsing singleton
   def parser
     @parse ||= OptionParser.new do |opts|
-      opts.banner = 'Usage: ./tools [command] <options>'
+      opts.banner = "Usage: ./tools [#{AVAILABLE_COMMANDS.join('|')}] <options>"
 
       # Allow user to choose a different configuration file
       opts.on('-c', '--config [path]', 'Change configuration file from default.') do |path|
